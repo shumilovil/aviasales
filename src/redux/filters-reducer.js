3a79cf239863dcb0943ef455cfb0cbc0b52ebab1
Il Shum <57085474+shumilovil@users.noreply.github.com>
2020-07-10 09:34:15 +0300

    Update

--- src/redux/filters-reducer.js
+++ src/redux/filters-reducer.js
@@ -1,11 +1,11 @@
 import { toogleSingleConnectionFilter, toogleAllConnectionFilter, changeDuration } from "./helpers/filters-reducer-helper";
 
 const TOGGLE_FILTER_THERE = 'TOGGLE_FILTER_THERE';
-const TOGGLE_FILTER_THERE_ALL_THERE = 'TOGGLE_FILTER_THERE_ALL_THERE';
+const TOGGLE_FILTER_ALL_THERE = 'TOGGLE_FILTER_ALL_THERE';
 const CHOOSE_DURATION_THERE = 'CHOOSE_DURATION_THERE';
 
 const TOGGLE_FILTER_FROM = 'TOGGLE_FILTER_FROM';
-const TOGGLE_FILTER_THERE_ALL_FROM = 'TOGGLE_FILTER_THERE_ALL_FROM';
+const TOGGLE_FILTER_ALL_FROM = 'TOGGLE_FILTER_ALL_FROM';
 const CHOOSE_DURATION_FROM = 'CHOOSE_DURATION_FROM';
 
 const initialState = {
@@ -46,7 +46,7 @@ const filtersReducer = (state = initialState, action) => {
         case TOGGLE_FILTER_THERE:
             return toogleSingleConnectionFilter(state, action, 'there');
 
-        case TOGGLE_FILTER_THERE_ALL_THERE:
+        case TOGGLE_FILTER_ALL_THERE:
             return toogleAllConnectionFilter(state, 'there');
 
         case CHOOSE_DURATION_THERE:
@@ -55,7 +55,7 @@ const filtersReducer = (state = initialState, action) => {
         case TOGGLE_FILTER_FROM:
             return toogleSingleConnectionFilter(state, action, 'from');
 
-        case TOGGLE_FILTER_THERE_ALL_FROM:
+        case TOGGLE_FILTER_ALL_FROM:
             return toogleAllConnectionFilter(state, 'from');
 
         case CHOOSE_DURATION_FROM:
@@ -70,11 +70,11 @@ const filtersReducer = (state = initialState, action) => {
 
 
 export const toggleFilterThere = (filter) => ({ type: TOGGLE_FILTER_THERE, filter });
-export const toggleallConnectionsThere = (filter) => ({ type: TOGGLE_FILTER_THERE_ALL_THERE, filter });
+export const toggleallConnectionsThere = (filter) => ({ type: TOGGLE_FILTER_ALL_THERE, filter });
 export const chooseDurationThere = (duration) => ({ type: CHOOSE_DURATION_THERE, duration });
 
 export const toggleFilterFrom = (filter) => ({ type: TOGGLE_FILTER_FROM, filter });
-export const toggleallConnectionsFrom = (filter) => ({ type: TOGGLE_FILTER_THERE_ALL_FROM, filter });
+export const toggleallConnectionsFrom = (filter) => ({ type: TOGGLE_FILTER_ALL_FROM, filter });
 export const chooseDurationFrom = (duration) => ({ type: CHOOSE_DURATION_FROM, duration });
 
 export default filtersReducer;
