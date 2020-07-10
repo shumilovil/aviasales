07fb65e15cd1a747d455c867cc606afd128473d8
Il Shum <57085474+shumilovil@users.noreply.github.com>
2020-07-10 10:07:39 +0300

    Format RangeSlider

--- src/components/Filters/RangeSlider/RangeSlider.jsx
+++ src/components/Filters/RangeSlider/RangeSlider.jsx
@@ -4,45 +4,45 @@ import Slider from '@material-ui/core/Slider';
 import style from './RangeSlider.module.css'
 
 const useStyles = makeStyles({
-  root: {
-    width: 190
-  }
+    root: {
+        width: 190
+    }
 });
 
 function valuetext(value) {
-  return `${value}`;
+    return `${value}`;
 }
 
 export default function RangeSlider(props) {
-  const classes = useStyles();
-  const [value, setValue] = React.useState([0, 50]);  
+    const classes = useStyles();
+    const [value, setValue] = React.useState([0, 50]);
 
-  useEffect(() => {
-    setValue([props.minMaxDuration.min, props.minMaxDuration.max])
-  }, [props.minMaxDuration])
+    useEffect(() => {
+        setValue([props.minMaxDuration.min, props.minMaxDuration.max])
+    }, [props.minMaxDuration])
 
-  useEffect(() => {
-    props.chooseDuration({ min: value[0] * 60, max: value[1] * 60 })
-  }, [value])
+    useEffect(() => {
+        props.chooseDuration({ min: value[0] * 60, max: value[1] * 60 })
+    }, [value])
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <div className={style.rangeSlider}>
-      <div className={classes.root}>
-        <div className={style.duration}>Время в пути, ч: от {value[0]} до {value[1]}</div>
-        <Slider
-          value={value}
-          onChange={handleChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-          getAriaValueText={valuetext}
-          min={props.minMaxDuration.min}
-          max={props.minMaxDuration.max}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className={style.rangeSlider}>
+            <div className={classes.root}>
+                <div className={style.duration}>Время в пути, ч: от {value[0]} до {value[1]}</div>
+                <Slider
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                    min={props.minMaxDuration.min}
+                    max={props.minMaxDuration.max}
+                />
+            </div>
+        </div>
+    );
 }
